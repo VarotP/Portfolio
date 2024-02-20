5857212730675a814e7316d4d3b25f8f8be34cb5
Val Pavaritpong <varotpava@gmail.com>
2024-02-19 18:43:02 -0800

    Added tool bar and font

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,17 +1,21 @@
-import { Button, Container } from '@mui/material'
+import { Button, Container, AppBar, Toolbar, Typography } from '@mui/material'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Varot Pavaritpong's Portfolio</h1>
-      <Container>
-        <Button>Home</Button>
-        <Button>Projects</Button>
-        <Button>Blog</Button>
-        <Button>Games</Button>
-        <Button>Contact</Button>
-      </Container>
+      <Typography style={{fontFamily: 'Satoshi', fontSize: '64px'}}>Varot Pavaritpong's Portfolio</Typography>
+      <AppBar style={{backgroundColor: 'purple'}}>
+        <Toolbar>
+          <Container style={{ display: 'flex', justifyContent: 'flex-end', }}>
+            <Button style={{ backgroundColor: 'black', color: 'white' }}>Projects</Button>
+            <Button style={{ backgroundColor: 'black', color: 'white', marginLeft: '20px' }}>Blog</Button>
+          </Container>
+        
+      
+        </Toolbar>
+      </AppBar>
+      
     </main>
   )
 }
